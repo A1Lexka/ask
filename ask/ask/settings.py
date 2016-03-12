63493911a97f43ccd296cfab4e7508f3a37e48ce
serg <serg.kolomiiets@gmail.com>
2016-03-12 06:48:18 +0000

    added gunicorn config

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'qa',
+    'ask',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -45,7 +46,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-#    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
 #    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('qa.urls')),
-#    url(r'^login/', include('qa.urls')),
-#    url(r'^signup/', include('qa.urls')),
-#    url(r'^question/', include('qa.urls')),
-#    url(r'^ask/', include('qa.urls')),
-#    url(r'^popular/', include('qa.urls')),
-#    url(r'^new/', include('qa.urls')),
+    url(r'^$', 'qa.views.test'),
+    url(r'^login/$', 'qa.views.test'),
+    url(r'^signup/$', 'qa.views.test'),
+    url(r'^question/\d+/$', 'qa.views.test'),
+    url(r'^ask/$', 'qa.views.test'),
+    url(r'^popular/$', 'qa.views.test'),
+    url(r'^new/$', 'qa.views.test),
 
 ]
