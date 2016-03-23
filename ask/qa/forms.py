af5b789ff015226093cfa84328cbde78fd29e3da
i <you@ex.com>
2016-03-23 09:13:16 +0000

    forms added

---
+++ ask/qa/forms.py
@@ -0,0 +1,41 @@
+form django import forms
+
+
+class AskForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    text = forms.CharField(widget=forms.Textarea)
+
+    def clean(self):
+        text = self.cleaned_data['text']
+        if not is_ethic(message):
+            raise forms.ValidationError('text is wrong', code=12)
+        return text + "Thank you."
+
+    def save(self):
+        text = Question(**self.cleaned_data)
+        text.save()
+        return text
+
+
+class AnswerForm(forms.Form):
+    text = forms.CharField(max_length=100)
+    question = forms.IntegerField()
+
+    def clean(self):
+            
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
